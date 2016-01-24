e814b218378057512150c40da3756263f8c8f285
Josh C <legomania@cox.net>
2016-01-24 10:30:31 -0800

    My way of testing buttons

--- RetroCalulator/ViewController.swift
+++ RetroCalulator/ViewController.swift
@@ -15,10 +15,49 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Outlets
+    @IBOutlet weak var OutputLbl: UILabel!
+    
+    //Vars
+    var Output:Double = 0.0
+    var OpSelected:String?
+    var LastOutput:Double = 0.0
+    
+    //Number Actions
+    @IBAction func Pressed0(sender: AnyObject) {
     }
+    @IBAction func Pressed1(sender: AnyObject) {
+    }
+    @IBAction func Pressed2(sender: AnyObject) {
+    }
+    @IBAction func Pressed3(sender: AnyObject) {
+    }
+    @IBAction func Pressed4(sender: AnyObject) {
+    }
+    @IBAction func Pressed5(sender: AnyObject) {
+    }
+    @IBAction func Pressed6(sender: AnyObject) {
+    }
+    @IBAction func Pressed7(sender: AnyObject) {
+    }
+    @IBAction func Pressed8(sender: AnyObject) {
+    }
+    @IBAction func Pressed9(sender: AnyObject) {
+    }
+    
+    //Operator Actions
+    @IBAction func add(sender: AnyObject) {
+    }
+    @IBAction func subtract(sender: AnyObject) {
+    }
+    @IBAction func multiply(sender: AnyObject) {
+    }
+    @IBAction func divide(sender: AnyObject) {
+    }
+    
+    //Functions
+    
+    
 
 
 }
